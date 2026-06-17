fe8d5fe90aea55aa0bd8d22946bdd3cee20a0c61
noarkhh <jakub.pryc@gmail.com>
2026-06-17 14:40:13 +0200

    Initial commit

---
+++ lib/goatmire2026_workshop.ex
@@ -0,0 +1,18 @@
+defmodule Goatmire2026Workshop do
+  @moduledoc """
+  Documentation for `Goatmire2026Workshop`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Goatmire2026Workshop.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/goatmire2026_workshop_test.exs
@@ -0,0 +1,8 @@
+defmodule Goatmire2026WorkshopTest do
+  use ExUnit.Case
+  doctest Goatmire2026Workshop
+
+  test "greets the world" do
+    assert Goatmire2026Workshop.hello() == :world
+  end
+end
