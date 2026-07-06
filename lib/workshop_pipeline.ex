d38733194f7f36df1d8808099974edde701068a6
noarkhh <jakub.pryc@gmail.com>
2026-07-06 16:09:40 +0200

    Finished task from Chapter 2

--- lib/workshop_pipeline.ex
+++ lib/workshop_pipeline.ex
@@ -30,17 +30,25 @@ defmodule WorkshopPipeline do
   # however we recommend using a dedicated State struct.
   @impl true
   def handle_init(_ctx, _opts) do
-    # :ivf_file_source --> :ivf_deserializer --> :video_transcoder
-    #                                                   |
-    #                                                   \/ 
-    #                                                :mp4_muxer --> :mp4_file_sink
-    #                                                   ^ 
-    #                                                   |
-    # :mp3_file_source ------------------------> :audio_transcoder
+    # :ivf_file_source --> :ivf_deserializer --> :video_decoder --> :color_inverter --> :video_encoder
+    #                                                                                         |
+    #                                                                                         \/
+    #                                                                                       :mp4_muxer --> :mp4_file_sink
+    #                                                                                         ^
+    #                                                                                         |
+    # :mp3_file_source ---------------------------------------------------------------> :audio_transcoder
     spec = [
       child(:ivf_file_source, %Membrane.File.Source{location: "assets/bbb_vp8.ivf"})
       |> child(:ivf_deserializer, Membrane.IVF.Deserializer)
       |> child(:video_transcoder, Membrane.Transcoder)
+      |> child(:video_decoder, Membrane.Transcoder)
+      |> via_out(:output,
+        options: [
+          output_stream_format: %Membrane.Transcoder.OutputFormat.RawVideo{pixel_format: :RGB}
+        ]
+      )
+      |> child(:color_inverter, ColorInverter)
+      |> child(:video_encoder, Membrane.Transcoder)
       |> via_out(:output,
         options: [
           output_stream_format: %Membrane.Transcoder.OutputFormat.H264{stream_structure: :avc1}
