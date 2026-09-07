0e6b352cc603936059cba8de3dde20cf004a4f81
Łukasz Kita <lukasz.kita0@gmail.com>
2026-09-07 15:03:52 +0200

    Task 3.2. Add StreamSwitcher

--- lib/workshop_pipeline.ex
+++ lib/workshop_pipeline.ex
@@ -4,6 +4,8 @@ defmodule WorkshopPipeline do
   """
   use Membrane.Pipeline
 
+  @switch_time Membrane.Time.seconds(15)
+
   require Membrane.Logger
 
   defmodule State do
@@ -30,9 +32,10 @@ defmodule WorkshopPipeline do
   # however we recommend using a dedicated State struct.
   @impl true
   def handle_init(_ctx, _opts) do
-    # :ivf_file_source --> :ivf_deserializer --> :video_decoder --> :color_inverter
-    #   --> :pixel_format_converter --> :realtimer --> :sdl_player
-    spec =
+    # :ivf_file_source --> :ivf_deserializer --> :video_decoder --> :color_inverter --> :stream_switcher
+    # :ad_ivf_file_source --> :ad_ivf_deserializer --> :ad_video_decoder ---------------------^
+    # :stream_switcher --> :pixel_format_converter --> :realtimer --> :sdl_player
+    spec = [
       child(:ivf_file_source, %Membrane.File.Source{location: "assets/bbb_vp8.ivf"})
       |> child(:ivf_deserializer, Membrane.IVF.Deserializer)
       |> child(:video_decoder, Membrane.Transcoder)
@@ -42,6 +45,8 @@ defmodule WorkshopPipeline do
         ]
       )
       |> child(:color_inverter, ColorInverter)
+      |> via_in(:main)
+      |> child(:stream_switcher, %StreamSwitcher{switch_time: @switch_time})
       |> child(:pixel_format_converter, Membrane.Transcoder)
       |> via_out(:output,
         options: [
@@ -49,7 +54,18 @@ defmodule WorkshopPipeline do
         ]
       )
       |> child(:realtimer, Membrane.Realtimer)
-      |> child(:sdl_player, Membrane.SDL.Player)
+      |> child(:sdl_player, Membrane.SDL.Player),
+      child(:ad_ivf_file_source, %Membrane.File.Source{location: "assets/ad_vp8.ivf"})
+      |> child(:ad_ivf_deserializer, Membrane.IVF.Deserializer)
+      |> child(:ad_video_decoder, Membrane.Transcoder)
+      |> via_out(:output,
+        options: [
+          output_stream_format: %Membrane.Transcoder.OutputFormat.RawVideo{pixel_format: :RGB}
+        ]
+      )
+      |> via_in(:ad)
+      |> get_child(:stream_switcher)
+    ]
 
     {[spec: spec], %State{}}
   end
