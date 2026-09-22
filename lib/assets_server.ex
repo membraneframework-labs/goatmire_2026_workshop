5d9ee616371540a68bf12b91529fe18d47be13a4
Łukasz Kita <lukasz.kita0@gmail.com>
2026-09-22 10:55:28 +0200

    Print the browser instructions from AssetsServer and use the unmodified Boombox page

--- lib/assets_server.ex
+++ lib/assets_server.ex
@@ -5,5 +5,22 @@ defmodule AssetsServer do
   plug(Plug.Static, at: "/", from: "assets")
   plug(:not_found)
 
+  def child_spec(opts) do
+    %{id: __MODULE__, start: {__MODULE__, :start_link, [opts]}}
+  end
+
+  def start_link(opts) do
+    port = Keyword.fetch!(opts, :port)
+
+    IO.puts("""
+
+    Open in your browser:
+      1. http://localhost:#{port}/webrtc_from_browser.html - sends your camera and microphone to the pipeline
+      2. http://localhost:#{port}/webrtc_to_browser.html   - plays the pipeline's output
+    """)
+
+    Bandit.start_link(plug: __MODULE__, port: port)
+  end
+
   def not_found(conn, _opts), do: Plug.Conn.send_resp(conn, 404, "Not found")
 end
--- run_pipeline.exs
+++ run_pipeline.exs
@@ -1,12 +1,3 @@
-http_port = 8000
-
-IO.puts("""
-
-Open in your browser:
-  1. http://localhost:#{http_port}/webrtc_from_browser.html - sends your camera and microphone to the pipeline
-  2. http://localhost:#{http_port}/webrtc_to_browser.html   - plays the pipeline's output
-""")
-
 {:ok, supervisor, _pipeline} = Membrane.Pipeline.start_link(WorkshopPipeline, [])
 
 Process.monitor(supervisor)
